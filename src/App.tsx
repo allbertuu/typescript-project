6bfbeadb40b4c51adfec3a41fd333fd155e096d4
Alberto <albertovinicius3@gmail.com>
2022-05-09 20:06:19 -0300

    feat(footer)
    add footer section

--- src/App.tsx
+++ src/App.tsx
@@ -177,6 +177,10 @@ function App() {
     <div className='container'>
       <h1>Bem-vindo, piloto 👨🏼‍✈️</h1>
       <button onClick={start}>Iniciar!</button>
+      <footer>
+        <p>Para eventuais problemas, ou se quiser me conhecer :)</p>
+        <a href="https://www.linkedin.com/albertov-albuquerque">LinkedIn do criador</a>
+      </footer>
     </div>
   );
 }
